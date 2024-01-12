2c9a3946442b85e0ed3cc79e6cb3807259d59178
Petri Pietikäinen <petrip@PetrisBeMacbook.intra.boogiesoftware.com>
2024-01-12 14:49:47 +0200

    basic lazy conf + neo-tree added

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+   })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- load config files
+require("config.options")
+require("config.mappings")
+
+-- load lazy and plugins
+require("lazy").setup("plugins")
+
