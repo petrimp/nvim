b14634c1d18e9f24ae58cebcc081ea0f9075d4b5
Petri Pietikäinen <petri.pietikainen@gmail.com>
2024-01-15 20:26:30 +0200

    add ui-select for telescope, lsp configurations, lualine

---
+++ lua/plugins/lsp-conf.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconf = require("lspconfig")
+      lspconf.lua_ls.setup({})
+      lspconf.tsserver.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
